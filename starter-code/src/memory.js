1307e3241df7b0aba0f4e60ac0dea61c490f119f
Torre <torre.locascio@gmail.com>
2018-01-30 18:31:24 -0500

    6pm commit unfinished

--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,15 +1,36 @@
-// var MemoryGame = function (cards) {
-//   this.cards = cards;
-// };
+var MemoryGame = function (cards) {
+  this.cards = cards;
+  this.pickedCards=[];
+  this.pairsClicked = 0;
+  this.pairsGuessed = 0;
+};
+MemoryGame.prototype.shuffleCard = function (array){
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
 
-// MemoryGame.prototype.shuffleCard = function (cardsArr) {
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
 
-// };
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-// MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-// }
+  return array;
+}
+}
 
-// MemoryGame.prototype.finished = function () {
+MemoryGame.prototype.checkIfPair = function(firstCard, secondCard){
+if (firstCard=== secondCard){true}else{false}
+}
 
-// };
+MemoryGame.prototype.finished = function(){if (pairsGuessed=pickedCards.length/2){
+  console.log("Congrats you Won!")} else {}
+}
+
+
